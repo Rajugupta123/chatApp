9fd3bd38be5a56cf84568bfd17d1a7c5daa6f76f
Rajugupta123 <raju.kgupta77@gmail.com>
2023-08-22 18:38:38 +0545

    chakra ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import {Box,Button,Container,HStack,Input,VStack} from "@chakra-ui/react"
+import Message from "./Components/Message"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box bg={"red.50"}>
+        <Container h={"100vh"} bg={"#fff"} >
+            <VStack h="full" paddingY={2} >
+                <Button colorScheme={"red"} w={"full"} >
+                    Logout
+                </Button>
+
+                <VStack h={"full"} w={"full"} overflowY={"auto"} >
+                    <Message text={"sample message"}/>
+                    <Message user="me" text={"sample message"}/>
+                    <Message text={"sample message"}/>
+                    <Message text={"sample message"}/>
+                    <Message  user="me" text={"sample message"}/>
+                    <Message text={"sample message"}/>
+                    <Message text={"sample message"}/>
+                    <Message  user="me" text={"sample message"}/>
+                    <Message text={"sample message"}/>
+                    <Message user="me" text={"sample message"}/>
+                    <Message text={"sample message"}/>
+                </VStack>
+
+                <form style={{width:"100%"}}>
+                    <HStack>
+                        <Input placeholder="Enter a message..."/>
+                        <Button type="submit" colorScheme="purple">
+                            send
+                        </Button>                        
+                    </HStack>
+                </form>
+
+
+            </VStack>
+        </Container>
+    </Box>
+  )
 }
 
-export default App;
+export default App
