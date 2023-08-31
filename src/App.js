28340440c412bac80a5fda7fdec643a328bcbf5f
Rajugupta123 <raju.kgupta77@gmail.com>
2023-08-31 23:10:03 +0545

    dynamic message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Box,Button,Container,HStack,Input,VStack} from "@chakra-ui/react"
 import Message from "./Components/Message"
 import { app } from "./firebase";
 import {onAuthStateChanged,getAuth,GoogleAuthProvider,signInWithPopup,signOut} from "firebase/auth"
-import {getFirestore,addDoc, collection, serverTimestamp} from "firebase/firestore"
+import {getFirestore,addDoc, collection, serverTimestamp,onSnapshot,query,orderBy} from "firebase/firestore"
 
 
 
@@ -16,26 +16,30 @@ const loginHandler =()=>{
 
     signInWithPopup(auth,provider)
 }
+
 //logout
 const logoutHandler = ()=> signOut(auth)
 
 
 const App = () => {
 
+    const q = query(collection(db,"Messages"),orderBy("text","asc"));
     const[user,setUser] = useState(false);
-    //console.log(user);
+    const[message,setMessage] = useState("");
+    const[messages,setMessages] = useState([]);
 
     //form submit handler
     const submitHandler = async(e)=>{
         e.preventDefault();
         try {
             await addDoc(collection(db,"Messages"),{
-                text:"adbs",
+                text:message,
                 user:user.uid,
                 uri:user.photoURL,
                 createdAt:serverTimestamp()
             
-            })
+            });
+        setMessage("");
 
         } catch (error) {
             alert(error);
@@ -44,11 +48,20 @@ const App = () => {
 
     useEffect(()=>{
         const unsubscribe = onAuthStateChanged(auth,(data)=>{
-        console.log(data);
+        //console.log(data);
         setUser(data);
     })
+
+    const unsubscribeForMessage =  onSnapshot(q,(snap)=>{
+        setMessages(snap.docs.map((item)=>{
+            const id = item.id;
+            return {id,...item.data()};
+        }));
+    })
+
     return ()=>{
         unsubscribe();
+        unsubscribeForMessage();
     }
 
     },[])
@@ -59,27 +72,28 @@ const App = () => {
             user?(
         <Container h={"100vh"} bg={"#fff"} >
             <VStack h="full" paddingY={2} >
-                <Button onClick={logoutHandler} colorScheme={"red"} w={"full"} >
+                <Button  value={message} onClick={logoutHandler} colorScheme={"red"} w={"full"} >
                     Logout
                 </Button>
 
                 <VStack h={"full"} w={"full"} overflowY={"auto"} >
-                    <Message text={"sample message"}/>
+                    {/* <Message text={"sample message"}/>
                     <Message user="me" text={"sample message"}/>
                     <Message text={"sample message"}/>
                     <Message text={"sample message"}/>
                     <Message  user="me" text={"sample message"}/>
-                    <Message text={"sample message"}/>
-                    <Message text={"sample message"}/>
-                    <Message  user="me" text={"sample message"}/>
-                    <Message text={"sample message"}/>
-                    <Message user="me" text={"sample message"}/>
-                    <Message text={"sample message"}/>
+                     */}
+
+                     {
+                        messages.map((item,index)=>(
+                            <Message user={item.uid === user.uid?"me":"other"} text={item.text} uri={item.uri} key={index}/>
+                        ))
+                     }
                 </VStack>
 
                 <form onSubmit={submitHandler} style={{width:"100%"}}>
                     <HStack>
-                        <Input placeholder="Enter a message..."/>
+                        <Input onChange={(e)=>setMessage(e.target.value)} placeholder="Enter a message..."/>
                         <Button type="submit" colorScheme="purple">
                             send
                         </Button>                        
