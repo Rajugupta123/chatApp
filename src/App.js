39676db6a987ad53f2579b229f5376461e744006
Rajugupta123 <raju.kgupta77@gmail.com>
2023-08-31 19:14:18 +0545

    firebase/firestore

--- src/App.js
+++ src/App.js
@@ -1,32 +1,57 @@
-import {Box,Button,Center,Container,HStack,Input,VStack} from "@chakra-ui/react"
-import Message from "./Components/Message"
-import {onAuthStateChanged,getAuth,GoogleAuthProvider,signInWithPopup,signOut} from "firebase/auth"
-import { app } from "./firebase";
 import { useEffect, useState } from "react";
+import {Box,Button,Container,HStack,Input,VStack} from "@chakra-ui/react"
+import Message from "./Components/Message"
+import { app } from "./firebase";
+import {onAuthStateChanged,getAuth,GoogleAuthProvider,signInWithPopup,signOut} from "firebase/auth"
+import {getFirestore,addDoc, collection, serverTimestamp} from "firebase/firestore"
+
+
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 
+//sign in with google
 const loginHandler =()=>{
     const provider = new GoogleAuthProvider();
 
     signInWithPopup(auth,provider)
 }
-
+//logout
 const logoutHandler = ()=> signOut(auth)
+
 
 const App = () => {
 
-  const[user,setUser] = useState(false);
-  
-  useEffect(()=>{
-    const unsubscribe = onAuthStateChanged(auth,(data)=>{
+    const[user,setUser] = useState(false);
+    //console.log(user);
+
+    //form submit handler
+    const submitHandler = async(e)=>{
+        e.preventDefault();
+        try {
+            await addDoc(collection(db,"Messages"),{
+                text:"adbs",
+                user:user.uid,
+                uri:user.photoURL,
+                createdAt:serverTimestamp()
+            
+            })
+
+        } catch (error) {
+            alert(error);
+        }
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth,(data)=>{
+        console.log(data);
         setUser(data);
     })
     return ()=>{
         unsubscribe();
     }
 
-  },[])
+    },[])
 
   return (
     <Box bg={"red.50"}>
@@ -52,7 +77,7 @@ const App = () => {
                     <Message text={"sample message"}/>
                 </VStack>
 
-                <form style={{width:"100%"}}>
+                <form onSubmit={submitHandler} style={{width:"100%"}}>
                     <HStack>
                         <Input placeholder="Enter a message..."/>
                         <Button type="submit" colorScheme="purple">
